3d401f344c7403c121a9eb145e1033c92bd76a52
IlnazG <wistfuldr@mail.com>
2020-04-08 18:36:08 +0300

    Added localization files

---
+++ src/main/java/itis/homework/demo/Config/LocalizationConfig.java
@@ -0,0 +1,60 @@
+package itis.homework.demo.Config;
+
+import itis.homework.demo.Dto.Message;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class LocalizationConfig implements WebMvcConfigurer {
+
+    // добавили в наше приложение перехватчик запросов для локализации
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    // храним информацию о выбранном языке в куках
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setCookieName("localeInfo");
+        cookieLocaleResolver.setCookieMaxAge(60 * 60 * 24 * 365);
+        return cookieLocaleResolver;
+    }
+
+    // перехватчик настроек языка
+    // то есть если на сервер пришел запрос localhost:8080/login?lang=ru или ?lang=en, то этот перехватчик
+    // установит куку с нужным значением
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    // откуда читать ключи с языками
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        DefaultMessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+        codesResolver.setMessageCodeFormatter(DefaultMessageCodesResolver.Format.POSTFIX_ERROR_CODE);
+        return codesResolver;
+    }
+}
+
